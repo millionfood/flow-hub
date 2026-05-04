3e52b942ead82d8edb4ffccfda42ea631d18bd60
million_food <million_food@naver.com>
2026-05-04 16:53:03 +0900

    chore: Spring Boot 초기 세팅 완료

---
+++ src/main/java/com/ajh/flow/FlowHubApplication.java
@@ -0,0 +1,13 @@
+package com.ajh.flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajh/flow/FlowHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajh.flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
