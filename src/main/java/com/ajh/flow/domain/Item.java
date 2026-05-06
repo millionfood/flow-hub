fc20700a6b0af6dc76ef585c18b83bb8cb3b1ddc
million_food <million_food@naver.com>
2026-05-07 00:14:32 +0900

    fix: createFUllBarcode 메서드의 로직에서 productCode의 값이 증가되지 않는 오류 수정

--- src/main/java/com/ajh/flow/domain/Item.java
+++ src/main/java/com/ajh/flow/domain/Item.java
@@ -1,14 +1,20 @@
 package com.ajh.flow.domain;
 
+import com.ajh.flow.common.constant.ItemUnit;
+import com.ajh.flow.common.constant.UseYn;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 @Entity
 @Getter @Setter
 @NoArgsConstructor
 public class Item extends BaseEntity {
+
+    private static String PREFIX = "8801111"; // 한국880 + 자회사1111
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,12 +29,46 @@ public class Item extends BaseEntity {
     @Column(nullable = false, length = 100)
     private String name; //상품명
 
+    @Enumerated(EnumType.STRING)
     @Column(length = 20)
-    private String unit; //단위
+    private ItemUnit unit; //단위
 
     @Column(length = 255)
     private String description; //상품설명
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private boolean useYn = true; // 사용 여부 (삭제 대신 상태값 변경)
+    private UseYn useYn = UseYn.Y; // 사용 여부 (삭제 대신 상태값 변경)
+
+    @Builder
+    public Item(String barcode, Long price, String name, ItemUnit unit, String description) {
+        this.barcode = barcode;
+        this.price = price;
+        this.name = name;
+        this.unit = unit;
+        this.description = description;
+    }
+
+    public void createFullBarcode(String productCode){
+        int nextCode = Integer.parseInt(productCode) + 1;
+        String formattedCode = String.format("%05d", nextCode);
+        String raw = PREFIX + formattedCode;
+        int checkDigit = calculateEan13CheckDigit(raw);
+        this.barcode = raw+checkDigit;
+    }
+
+    private int calculateEan13CheckDigit(String raw){
+        int sum = 0;
+        for(int i = 0; i < 12; i++){
+            int digit = raw.charAt(i) - '0';
+            if(i%2 ==  1){
+                sum += digit * 3;
+            }else{
+                sum += digit;
+            }
+        }
+        int remainder = sum % 10;
+
+        return (10-remainder) %10;
+    }
 }
