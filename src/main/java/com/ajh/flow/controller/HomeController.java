c93cd05d761630fd93bb1b2103c582c4a8e79cd3
million_food <million_food@naver.com>
2026-05-28 14:02:25 +0900

    refactor: 기존의 메인페이지 제거 후 첫 화면부터 관리자/ 일반유저 권한에 맞는 콘솔로 이동하도록 분기

--- src/main/java/com/ajh/flow/controller/HomeController.java
+++ src/main/java/com/ajh/flow/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.ajh.flow.controller;
 
+import com.ajh.flow.domain.PrincipalDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String home(Model model) {
-        return "home";
+    public String home(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+        if(principalDetails == null){
+            return "redirect:/user/login";
+        }
+        if(principalDetails.getAuthorities().stream()
+                .anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMIN"))) {
+            return "redirect:/admin/home";
+        }
+        return "redirect:/user/home";
     }
 }
